5823ec7acdd8808549355c4d74331bd24f214fc7
NurullahSanY <alaminkhan10009@gmail.com>
2024-03-15 22:45:17 +0600

    flowerspost
    end

--- front-end/src/app/flowerspost/flowerspost.component.ts
+++ front-end/src/app/flowerspost/flowerspost.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FaConfig } from '@fortawesome/angular-fontawesome';
+import { faPhone, faEnvelope,faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookSquare,
+  faTwitterSquare,
+  faInstagramSquare,
+  faPinterestSquare,
+  
+  
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-flowerspost',
@@ -6,5 +16,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./flowerspost.component.css']
 })
 export class FlowerspostComponent {
+  homeicon = faLocationDot;
+  emailicon = faEnvelope;
+  phoneicon = faPhone;
+  facebookicon = faFacebookSquare;
+  tweetrericon = faTwitterSquare;
+  pinteresticon = faPinterestSquare;
+  instagramicon = faInstagramSquare;
 
 }
